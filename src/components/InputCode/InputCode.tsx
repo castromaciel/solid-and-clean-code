3998a5eb0004fbcbf552a15b345a0db36b454cfb
castromaciel <castromaciel00@gmail.com>
2023-02-25 22:52:44 -0300

    change syntaxhighlighter

--- src/components/InputCode/InputCode.tsx
+++ src/components/InputCode/InputCode.tsx
@@ -1,6 +1,6 @@
 import { FC, ReactNode, useContext } from 'react'
-import { PrismLight as SyntaxHighLighter } from 'react-syntax-highlighter'
-import { dracula, oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dracula, duotoneLight } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import { ThemeContext } from '../../provider'
 
 interface InputCodeProps {
@@ -11,13 +11,13 @@ const InputCode: FC<InputCodeProps> = ({ children }) => {
   const { isDark } = useContext(ThemeContext)
 
   return (
-    <SyntaxHighLighter
+    <SyntaxHighlighter
       customStyle={{ boxShadow: '1px 10px 76px -27px rgba(0,0,0,0.36)' }}
-      style={isDark ? dracula : oneLight}
+      style={isDark ? dracula : duotoneLight}
       language="typescript"
     >
       { String(children) }
-    </SyntaxHighLighter>
+    </SyntaxHighlighter>
   )
 }
 
